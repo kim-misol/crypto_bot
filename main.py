8a89232fa4fc3ebc0ffe2d746985655840dbc4c9
kim-misol <wonderfulpine@gmail.com>
2020-12-23 12:51:07 +0900

    Solved python convention

--- main.py
+++ main.py
@@ -38,37 +38,42 @@ def returns(book):
         # long 매수
         if book.loc[i, 'trade'] == 'buy' and book.shift(1).loc[i, 'trade'] == '':
             buy = book.loc[i, 'Close']
-            print('매수 날짜 : ', i, ' long 매수 가격 : ', buy)  # long 매도
+            print('매수 날짜 : ', i, ' 매수 가격 : ', buy)
+        # long 매도
         elif book.loc[i, 'trade'] == '' and book.shift(1).loc[i, 'trade'] == 'buy':
             sell = book.loc[i, 'Close']
             rtn = (sell - buy) / buy + 1  # 손익 계산
             book.loc[i, 'return'] = rtn
-            print('매도 날짜 : ', i, ' long 매수 가격 : ', buy, ' | long 매도 가격 : ', sell, ' | return :', round(rtn, 4))
+            print('매도 날짜 : ', i, ' 매수 가격 : ', buy, ' | 매도 가격 : ', sell, ' | return :', round(rtn, 4))
+
         if book.loc[i, 'trade'] == '':  # 제로 포지션
             buy = 0.0
             sell = 0.0
-            
+
     acc_rtn = 1.0
     for i in book.index:
         rtn = book.loc[i, 'return']
         acc_rtn = acc_rtn * rtn  # 누적 수익률 계산
         book.loc[i, ' acc return '] = acc_rtn
-        print(' Accunulated return :', round(acc_rtn, 4))
-        
+    print(' Accunulated return :', round(acc_rtn, 4))
+
     return round(acc_rtn, 4)
 
 
-price_df = fdr.DataReader('BTC/KRW', '2018')  # 비트코인 원화 가격 (빗썸) 2016년~현재
-# 결측치 존재 유무 확인
-invalid_data_cnt = len(price_df[price_df.isin([np.nan, np.inf, -np.inf]).any(1)])
-if invalid_data_cnt == 0:
-    df = price_df.loc[:, ['Close']].copy()
-    bb_df = bollinger_band(df)
+if __name__ == "__main__":
+    price_df = fdr.DataReader('BTC/KRW', '2018')  # 비트코인 원화 가격 (빗썸) 2016년~현재
+    # 결측치 존재 유무 확인
+    invalid_data_cnt = len(price_df[price_df.isin([np.nan, np.inf, -np.inf]).any(1)])
 
-    base_date = '2018-01-01'
-    sample = bb_df.loc[base_date:]
-    # 볼린더밴드 지표 수치 추가
-    # print(sample.tail(10))
-    book = create_trade_book(sample)
-    book = tradings(sample, book)
-    print(book.tail(100))
+    if invalid_data_cnt == 0:
+        df = price_df.loc[:, ['Close']].copy()
+        bb_df = bollinger_band(df)
+
+        base_date = '2018-01-01'
+        sample = bb_df.loc[base_date:]
+        # 볼린더밴드 지표 수치 추가
+        # print(sample.tail(10))
+        book = create_trade_book(sample)
+        book = tradings(sample, book)
+        earn = returns(book)
+        print(book.tail(100))
