Fixed to drop data included nan from moving average in preprocessing

back_trading.py
@@ -210,23 +210,20 @@ def ai_filter(coin_df):
     # 학습, 검증, 테스트 데이터 기간 분할 6:2:2
     train_df, val_df, test_df = data_split(coin_df)
     # 최소-최대 정규화
-    # ? nan 값이 있는 경우 어떻게 학습?
     train_sample_df, eng_list = min_max_normal(train_df)
+    # n일 이동평균값 이용 시 nan 값 있는 데이터 제거, nan 값 제거 후 model.fit 할 때 loss도 정상 출력, 에측값도 정상
+    nan_cnt = np.where(np.isnan(train_sample_df))[0][-1] + 1
+    train_sample_df = train_sample_df[nan_cnt:]
     val_sample_df, eng_list = min_max_normal(val_df)
     test_sample_df, eng_list = min_max_normal(test_df)
     # 레이블링 테이터
     # (num_step)일치 (n_feature)개 변수의 데이터를 사용해 다음날 종가 예측
     num_step = 5
     num_unit = 200
-    # ? 1을 왜 빼지?
-    # n_feature = len(eng_list) - 1
     n_feature = len(eng_list)
 
     # 훈련, 검증, 테스트 데이터를 변수 데이터와 레이블 데이터로 나눈다
     x_train, y_train = create_dataset_binary(train_sample_df, eng_list, num_step, n_feature)
-    # 20일 이동평균값 이용시 nan 값이 들어가서. nan 값 있는 데이터 삭제 필요, nan 값 빼니까 model.fit 할 때 loss도 정상 출력
-    x_train = x_train[19:]
-    y_train = y_train[19:]
     x_val, y_val = create_dataset_binary(val_sample_df, eng_list, num_step, n_feature)
     x_test, y_test = create_dataset_binary(test_sample_df, eng_list, num_step, n_feature)
 