48097e9e7cfc01dfc3f1b08274b6b0e809050215
kim-misol <wonderfulpine@gmail.com>
2021-01-21 17:45:59 +0900

    fixed error of "Shapes (None, 2) and (None, 5, 2) are incompatible"

--- back_trading.py
+++ back_trading.py
@@ -11,7 +11,9 @@ import pandas_datareader as pdr
 from graphs import draw_candle, draw_candle_with_indicator, plot_model_fit_history
 from data.code_list import stock_codes, coin_codes
 from trading_indicators import bollinger_band
-from ai import data_split, min_max_normal, create_dataset_binary, create_model, predict
+from ai import data_split, min_max_normal, create_dataset_binary, create_model, back_testing
+# from ai_model import min_max_normal, create_dataset_binary, create_model, back_testing
+# from ai import data_split
 
 
 class TestStrategy(bt.Strategy):
@@ -127,14 +129,15 @@ def data_settings(code, start=datetime(2020, 1, 1), end=datetime.today()):
     invalid_data_cnt = len(price_df[price_df.isin([np.nan, np.inf, -np.inf]).any(1)])
 
     if invalid_data_cnt == 0:
-        price_df['Date'] = price_df.index
+        # price_df['Date'] = price_df.index
         price_df['Open'] = price_df.iloc[:]['Open'].astype(np.float64)
         price_df['High'] = price_df.iloc[:]['High'].astype(np.float64)
         price_df['Low'] = price_df.iloc[:]['Low'].astype(np.float64)
         price_df['Close'] = price_df.iloc[:]['Close'].astype(np.float64)
         price_df['Volume'] = price_df.iloc[:]['Volume']
         price_df = bollinger_band(price_df)
-        df = price_df.loc[:, ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'ubb', 'mbb', 'lbb']].copy()
+        # df = price_df.loc[:, ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'ubb', 'mbb', 'lbb']].copy()
+        df = price_df.loc[:, ['Open', 'High', 'Low', 'Close', 'Volume', 'ubb', 'mbb', 'lbb']].copy()
         return df
     return False
 
@@ -212,26 +215,31 @@ def ai_filter(coin_df):
     # df['next_price'] = df['Adj Close'].shift(-1)
     coin_df['next_rtn'] = coin_df['Close'] / coin_df['Open'] - 1
     train_df, val_df, test_df = data_split(coin_df)
+    all_features = ['Open', 'High', 'Low', 'Close', 'Volume', 'ubb', 'mbb', 'lbb', 'next_rtn']
+    feature4_list = ['next_rtn']
     # 최소-최대 정규화
-    train_sample_df, eng_list = min_max_normal(train_df)
+    train_sample_df, eng_list = min_max_normal(train_df, all_features, feature4_list)
     # n일 이동평균값 이용 시 nan 값 있는 데이터 제거, nan 값 제거 후 model.fit 할 때 loss도 정상 출력, 에측값도 정상
     nan_cnt = np.where(np.isnan(train_sample_df))[0][-1] + 1
     train_sample_df = train_sample_df[nan_cnt:]
-    val_sample_df, eng_list = min_max_normal(val_df)
-    test_sample_df, eng_list = min_max_normal(test_df)
+
+    val_sample_df, eng_list = min_max_normal(val_df, all_features, feature4_list)
+    test_sample_df, eng_list = min_max_normal(test_df, all_features, feature4_list)
     # 레이블링 테이터
     # (num_step)일치 (n_feature)개 변수의 데이터를 사용해 다음날 종가 예측
     num_step = 5
     num_unit = 200
-    eng_list = eng_list + ['next_rtn']
-    n_feature = len(eng_list) - 1
+    eng_list = eng_list + feature4_list
+    n_feature = len(eng_list) - 1   # next_rtn 을 제외한 feature 개수
 
     # 훈련, 검증, 테스트 데이터를 변수 데이터와 레이블 데이터로 나눈다
     x_train, y_train = create_dataset_binary(train_sample_df, eng_list, num_step, n_feature)
     x_val, y_val = create_dataset_binary(val_sample_df, eng_list, num_step, n_feature)
     x_test, y_test = create_dataset_binary(test_sample_df, eng_list, num_step, n_feature)
     # ?뭐지.. github 찾아보니 책에는 생략되어있던 코드, 어쩐지 Dense(2)로 넣으면 shape이 달라서 에러가 나더라.
+    # create_dataset_binary 함수내에 포함 시켜야겠다
     from tensorflow.keras.utils import to_categorical
+
     y_train = to_categorical(y_train, 2)
     y_val = to_categorical(y_val, 2)
     y_test = to_categorical(y_test, 2)
@@ -241,15 +249,21 @@ def ai_filter(coin_df):
     # ? 모델 저장하여 재사용 가능한지 확인
     # model 학습. 휸련데이터샛을 이용해 epochs만큼 반복 훈련 (논문에선 5000으로 설정). verbose 로그 출력 설정
     # validation_data를 총해 에폭이 끝날 때마다 학습 모델을 해당 데이터로 평가한다. 해당 데이터로 학습하지는 않는다.
-    EPOCHS = 20
+    EPOCHS = 100
     BATCH_SIZE = 10
-    VERBOSE = 1
-    history = model.fit(x_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=VERBOSE,
-                        validation_data=(x_val, y_val))
+    history = model.fit(x_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, validation_data=(x_val, y_val))
     fig = plot_model_fit_history(history, EPOCHS)
     fig.show()
     # 내일 오를지 내릴지에 대한 label 예측 값 출력
-    future_price = predict(x_test, y_test, model)
+    predicted = model.predict(x_test)
+    y_pred = np.argmax(predicted, axis=1)
+    Y_test = np.argmax(y_test, axis=1)
+    print(f"")
+    print(f"{y_pred}\n{len(y_pred)}")
+    print(f"{Y_test}\n{len(Y_test)}")
+
+    back_testing(test_sample_df, y_pred)
+    # 모델 리턴해서 내일 오른다는 예측이 있을 경우 매수 떨어지면 매도
 
 
 if __name__ == "__main__":
