73cf38267244c154daf30ddc8ddf31197defd972
kim-misol <wonderfulpine@gmail.com>
2021-03-09 18:05:32 +0900

    ai_setting 조건 자동생성하는 코드 수정

--- create_ai_settings.py
+++ create_ai_settings.py
@@ -1,21 +1,22 @@
 import itertools
 
 # s_list = []
-simul_num = 'elif ai_filter_num == {i}:\nai_settings = {\n\t'
-table = '"table": "min_craw",\n'
-num_step = ['"num_step": 5,\n"num_units": 200,\n"epochs": 50,', '"num_step": 100,\n"num_units": 200,\n"epochs": 50,']
-batch_size = ['"batch_size": 10,\n"learning_rate": 0.001,\n"optimizer": "adam",', '"batch_size": 32,\n"learning_rate": 0.001,\n"optimizer": "adam",']
-loss = ['"loss": "categorical_crossentropy",', '"loss": "mse",', '"loss": "binary_crossentropy",']
-activation = ['"activation": "sigmoid",\n"is_continuously_train": False', '"activation": "softmax",\n"is_continuously_train": False', '"activation": "tanh",\n"is_continuously_train": False',
-              '"activation": "relu",\n"is_continuously_train": False', '"activation": "softsign",\n"is_continuously_train": False', '"activation": "softplus",\n"is_continuously_train": False']
+simul_num = ['elif ai_filter_num == {i}:\n\tai_settings = {']
+table = ['\t"table":  f"min{unit}",']
+num_step = ['\t"num_step": 5,\n\t"num_units": 200,\n\t"epochs": 50,']
+batch_size = ['\t"batch_size": 32,\n\t"learning_rate": 0.001,\n\t"optimizer": "adam",']
+loss = ['\t"loss": "categorical_crossentropy",', '\t"loss": "mse",', '\t"loss": "binary_crossentropy",']
+activation = ['\t"activation": "sigmoid",\n\t"is_continuously_train": False',
+              '\t"activation": "softmax",\n\t"is_continuously_train": False',
+              '\t"activation": "tanh",\n\t"is_continuously_train": False',
+              '\t"activation": "relu",\n\t"is_continuously_train": False',
+              '\t"activation": "softsign",\n\t"is_continuously_train": False',
+              '\t"activation": "softplus",\n\t"is_continuously_train": False']
 
-
-result = list(itertools.product(*[num_step,batch_size, loss, activation]))
-print(len(result))
-# print(result)
+result = list(itertools.product(*[simul_num, table, num_step, batch_size, loss, activation, ['}']]))
 
 for r in result:
     for rr in r:
         print(rr)
     print("\n")
-
+print(len(result))
