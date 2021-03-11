a19b680ce163ac21cd7ee7fb0d96375db73ae6e9
kim-misol <wonderfulpine@gmail.com>
2021-03-11 18:13:39 +0900

    model 학습 조건 추가

--- library/ai_setting_list.py
+++ library/ai_setting_list.py
@@ -507,6 +507,245 @@ def get_ai_settings(ai_filter_num, min_unit):
             "activation": "sigmoid",
             "is_continuously_train": False
         }
+
+    # step 5 units 200 epoch 50 batch 32 learning_rate 0.001
+    elif ai_filter_num == 1101:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "categorical_crossentropy",
+            "activation": "sigmoid",
+            "is_continuously_train": False
+        }
+    elif ai_filter_num == 1102:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "categorical_crossentropy",
+            "activation": "softmax",
+            "is_continuously_train": False
+        }
+    elif ai_filter_num == 1103:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "categorical_crossentropy",
+            "activation": "tanh",
+            "is_continuously_train": False
+        }
+    # 제외) 50, 5, 200, 32, 0.01, adam, categorical_crossentropy, relu loss값이 nan으로 나온다
+    elif ai_filter_num == 1104:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "categorical_crossentropy",
+            "activation": "relu",
+            "is_continuously_train": False
+        }
+    elif ai_filter_num == 1105:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "categorical_crossentropy",
+            "activation": "softsign",
+            "is_continuously_train": False
+        }
+    elif ai_filter_num == 1106:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "categorical_crossentropy",
+            "activation": "softplus",
+            "is_continuously_train": False
+        }
+    elif ai_filter_num == 1107:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "mse",
+            "activation": "sigmoid",
+            "is_continuously_train": False
+        }
+    elif ai_filter_num == 1108:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "mse",
+            "activation": "softmax",
+            "is_continuously_train": False
+        }
+    elif ai_filter_num == 1109:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "mse",
+            "activation": "tanh",
+            "is_continuously_train": False
+        }
+    elif ai_filter_num == 1110:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "mse",
+            "activation": "relu",
+            "is_continuously_train": False
+        }
+    elif ai_filter_num == 1111:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "mse",
+            "activation": "softsign",
+            "is_continuously_train": False
+        }
+    elif ai_filter_num == 1112:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "mse",
+            "activation": "softplus",
+            "is_continuously_train": False
+        }
+    elif ai_filter_num == 1113:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "binary_crossentropy",
+            "activation": "sigmoid",
+            "is_continuously_train": False
+        }
+    elif ai_filter_num == 1114:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "binary_crossentropy",
+            "activation": "softmax",
+            "is_continuously_train": False
+        }
+    elif ai_filter_num == 1115:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "binary_crossentropy",
+            "activation": "tanh",
+            "is_continuously_train": False
+        }
+    #  주식에서 정확도 1위
+    elif ai_filter_num == 1116:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "binary_crossentropy",
+            "activation": "relu",
+            "is_continuously_train": False
+        }
+    elif ai_filter_num == 1117:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "binary_crossentropy",
+            "activation": "softsign",
+            "is_continuously_train": False
+        }
+    elif ai_filter_num == 1118:
+        ai_settings = {
+            "table": f"min{min_unit}",
+            "num_step": 5,
+            "num_units": 200,
+            "epochs": 50,
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "optimizer": "adam",
+            "loss": "binary_crossentropy",
+            "activation": "softplus",
+            "is_continuously_train": False
+        }
+
     # 테스트용 1
     elif ai_filter_num == 999:
         ai_settings = {
